a163013b3e93aeabbe36e4c0733781a9aa0abc45
abhat3 <abhat3@GOT5CG74769RV.vccnet.volvocars.net>
2019-03-21 15:19:07 +0100

    Anita comminting

---
+++ TechmTraining/src/com/training/MyClass.java
@@ -0,0 +1,11 @@
+package com.training;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
