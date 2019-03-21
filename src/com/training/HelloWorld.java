cecbc82487ee6169cc2cc1ad0d161903bec0f67c
Fayaaz <mfayaazu@volvocars.com>
2019-03-21 15:57:45 +0100

    some changes

--- src/com/training/HelloWorld.java
+++ src/com/training/HelloWorld.java
@@ -1,11 +1,9 @@
 package com.training;
 
-import java.util.Scanner;
-
-public class Abhinav {
+public class HelloWorld {
 
 	
 	public static void main(String[] args) {
-	system.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
